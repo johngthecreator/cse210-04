a2ea6793ba505f5793225f2c7e337740602c9bb9
JT Clarke <jacksonclarke2001@hotmail.com>
2022-11-01 15:41:59 -0600

    practice code

---
+++ Game/Casting/Artifact.cs
@@ -0,0 +1,38 @@
+namespace Unit04.Game.Casting
+{
+    /// <summary>
+    /// <para>An item of cultural or historical interest.</para>
+    /// <para>
+    /// The responsibility of an Artifact is to provide a message about itself.
+    /// </para>
+    /// </summary>
+    public class Artifact : Actor
+    {
+        private string _message = "";
+
+        /// <summary>
+        /// Constructs a new instance of an Artifact.
+        /// </summary>
+        public Artifact()
+        {
+        }
+
+        /// <summary>
+        /// Gets the artifact's message.
+        /// </summary>
+        /// <returns>The message.</returns>
+        public string GetMessage()
+        {
+            return _message;
+        }
+
+        /// <summary>
+        /// Sets the artifact's message to the given value.
+        /// </summary>
+        /// <param name="message">The given message.</param>
+        public void SetMessage(string message)
+        {
+            this._message = message;
+        }
+    }
+}
